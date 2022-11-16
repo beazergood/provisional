3177683500e2e73d49b4cadff001008541ff4306
Dave Beazer <dave@sideprojectco.com>
2022-11-16 22:55:10 +0000

    add Storybook

---
+++ provisional/stories/pages/home.stories.tsx
@@ -0,0 +1,9 @@
+import Home from "../../pages/index";
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  title: "Pages/Home",
+  component: Home,
+};
+
+export const HomePage = () => <Home />
