8d46c07b543663b89c06434e0871bcc3281cd52f
Dave Beazer <dave@sideprojectco.com>
2022-11-28 10:51:01 +0000

    delete blank story file

--- stories/pages/home.stories.tsx
+++
@@ -1,9 +0,0 @@
-// import Courses from "../../pages/index";
-
-// /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-// export default {
-//   title: "Pages/Home",
-//   component: Courses,
-// };
-
-// export const HomePage = () => <Courses />
