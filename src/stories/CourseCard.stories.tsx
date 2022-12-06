3a703817b24e3caa45f165e2888c4686bd0a9e6e
Dave Beazer <dave@sideprojectco.com>
2022-12-06 22:37:48 +0000

    feat: wrap story in chakra provider

--- src/stories/CourseCard.stories.tsx
+++ src/stories/CourseCard.stories.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
-import CourseCard from '../components/CourseCard/CourseCard.view';
+import theme from '../theme'
+
+import CourseCard from '../components/CourseCard/CourseCard.view'
 
 export default {
-    title: 'Components/CourseCard',
-    component: CourseCard,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-      backgroundColor: { control: 'color' },
-    },
-  } as ComponentMeta<typeof CourseCard>;
-  
-  const Template: ComponentStory<typeof CourseCard> = (args) => <CourseCard {...args} />;
+  title: 'Components/CourseCard',
+  component: CourseCard,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof CourseCard>
 
-export const Main = Template.bind({});
+const Template: ComponentStory<typeof CourseCard> = (args) => (
+  <ChakraProvider theme={theme}>
+    <CourseCard {...args} />
+  </ChakraProvider>
+)
+
+export const Main = Template.bind({})
 
 Main.args = {
-  name: "Pronghorn Club",
-  city: "Bend",
-  state: "OR",  
+  name: 'Pronghorn Club',
+  city: 'Bend',
+  state: 'OR',
   imgUrl: 'images/pronghorn.jpg',
-};
+}
